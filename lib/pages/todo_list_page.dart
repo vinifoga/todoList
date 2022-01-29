fd7b01ea27706254e5075079e3c14e3a37ad93a6
Vinicius Fogaca <viniciusfernandes38@gmail.com>
2022-01-29 12:27:53 -0300

    Initial Layout

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -5,6 +5,64 @@ class TodoListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.yellow,);
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                children: [
+                  const Expanded(
+                    flex: 4,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Adicione um tarefa',
+                        hintText: 'Estudar Flutter',
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color(0xff00D7F3),
+                      padding: const EdgeInsets.all(15),
+                    ),
+                    child: const Icon(
+                      Icons.add,
+                      size: 30,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16,),
+              Row(
+                children: [
+                  const Expanded(
+                    child: Text(
+                      'Você possui 0 tarefas pendentes',
+                    ),
+                  ),
+                  const SizedBox(width: 8,),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color(0xff00D7F3),
+                      padding: const EdgeInsets.all(14),
+                    ),
+                    child: const Text('Limpar tudo'),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
